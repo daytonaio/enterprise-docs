95b7f224a93116d67566f266f832a1d1b471f4a6
stefanicjuraj <jurajstefanic@outlook.com>
2024-10-27 02:05:14 +0200

    docs: comment content
    Signed-off-by: stefanicjuraj <jurajstefanic@outlook.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -206,13 +206,13 @@ export default defineConfig({
                 icon: 'backstage.svg'
               },
             },
-            {
-              label: 'AI Code Assistant',
-              link: '/tools/ai-code-assistant',
-              attrs: {
-                icon: 'continue.svg',
-              },
-            },
+            // {
+            //   label: 'AI Code Assistant',
+            //   link: '/tools/ai-code-assistant',
+            //   attrs: {
+            //     icon: 'continue.svg',
+            //   },
+            // },
           ]
         },
         {
